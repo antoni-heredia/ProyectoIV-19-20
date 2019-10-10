cd12c56ab092fd838bb380398d682775cc546fd0
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2019-10-10 14:13:37 +0200

    cambiado test para revisar mime type

--- test/test_img.py
+++ test/test_img.py
@@ -1,10 +1,14 @@
 
 # content of test_expectation.py
 import pytest
+import mimetypes
+import requests
 
 
 
-@pytest.mark.parametrize("nombreFichero", ["datos","imagen.txt","texto.jpg","wally.bmp"])
-def test_imagen(nombreFichero):
-    
-    assert nombreFichero.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+
+@pytest.mark.parametrize("url", ["https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555285691/shape/mentalfloss/waldomain.png","https://miro.medium.com/max/1500/1*yO4EVVeBHYIrxKXDGY_-zw.jpeg"])
+def test_imagen(url):
+    response = requests.get(url, stream=True)
+    assert response.headers['Content-Type'].split("/")[0] == "image"
+
