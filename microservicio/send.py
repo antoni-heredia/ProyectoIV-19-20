718252f8d141b233a6c415c476cd0308391d2f38
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2019-11-18 18:53:55 +0100

    arreglado sendder

--- microservicio/send.py
+++ microservicio/send.py
@@ -4,14 +4,13 @@ from flask import jsonify
 
 def encolar(imagen, nombre):
     if not 'HEROKU' in os.environ:
-        logging.basicConfig()
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
     else:
         url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
         params = pika.URLParameters(url)
-        connection = pika.BlockingConnection(params)    channel = connection.channel()
-
+        connection = pika.BlockingConnection(params)
+        
     channel.queue_declare(queue='colaImagenes')
     channel.basic_publish(exchange='', routing_key='colaImagenes', body=nombre+":"+imagen)
 
