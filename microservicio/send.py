432a6e7643b198e48cd5d260ebe86f9b0ae243d7
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2019-11-18 18:57:55 +0100

    faltaba os en send

--- microservicio/send.py
+++ microservicio/send.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pika
 from flask import jsonify
-
+import os
 def encolar(imagen, nombre):
     if not 'HEROKU' in os.environ:
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -10,7 +10,7 @@ def encolar(imagen, nombre):
         url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
         params = pika.URLParameters(url)
         connection = pika.BlockingConnection(params)
-        
+
     channel.queue_declare(queue='colaImagenes')
     channel.basic_publish(exchange='', routing_key='colaImagenes', body=nombre+":"+imagen)
 
